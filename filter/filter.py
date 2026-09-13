5fe528c432f2600a1589bae4fe7a1f8fadb9b709
rozaqnh168 <rozaqnh1685@gmail.com>
2026-09-13 17:08:32 +0700

    Add filtered webcam script and image

--- filter/filter.py
+++ filter/filter.py
@@ -1,44 +1,55 @@
 import cv2
+from matplotlib import pyplot as plt
 
-# =========================
-# 1. BACA GAMBAR
-# =========================
 
+"""
 gambar = cv2.imread("gambar.jpg")
+gmbtype = gambar.dtype
+(w,h,c) = gambar.shape
 
+for i in range(w):
+    for j in range(h):
+        # Lapisan Green = 0
+        gambar[i,j,1]=0
+        # Lapisan Blue = 0
+        gambar[i,j,0]=0
+        #Lapisan Red = 0
+        #gambar[i,j,2]=0
 
-# =========================
-# 2. GRAYSCALE
-# =========================
+plt.imshow(gambar)
+plt.title("Gambar")
+#plt.show()
 
-gray = cv2.cvtColor(gambar, cv2.COLOR_BGR2GRAY)
-
-
-# =========================
-# 3. RED FILTER
-# =========================
-
-red = gambar.copy()
-
-red[:, :, 0] = 0
-red[:, :, 1] = 0
-
-
-# =========================
-# 4. TAMPILKAN
-# =========================
-
-cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Grayscale", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Red Filter", cv2.WINDOW_NORMAL)
-
-cv2.resizeWindow("Original", 800, 600)
-cv2.resizeWindow("Grayscale", 800, 600)
-cv2.resizeWindow("Red Filter", 800, 600)
-
-cv2.imshow("Original", gambar)
-cv2.imshow("Grayscale", gray)
-cv2.imshow("Red Filter", red)
-
+cv2.imshow("Gambar", gambar)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
+"""
+#Video Webcam
+
+kamera = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = kamera.read()
+    (w, h, c) = frame.shape
+
+    # FILTER
+    for i in range(w):
+        for j in range(h):
+            frame[i,j,1] = 0
+            frame[i,j,0] = 0
+
+    cv2.imshow("Video", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 27:
+        break
+    try :
+        if cv2.getWindowProperty("Video", cv2.WND_PROP_VISIBLE) < 1:
+         break
+    except cv2.error:
+        break
+
+kamera.release()
 cv2.destroyAllWindows()
